b92d80a99affad3481ff9d76262305e44900cd19
quinnw10 <jquinnw10@gmail.com>
2025-01-02 12:02:37 -0500

    updated package.json

--- Develop/server/dist/seeds/cleanDb.js
+++ Develop/server/dist/seeds/cleanDb.js
@@ -2,6 +2,12 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 export default async (modelName, collectionName) => {
     try {
+        if (!models[modelName] || !models[modelName].db) {
+            throw new Error(`Model or database not found for modelName: ${modelName}`);
+        }
+        if (!models[modelName].db.db) {
+            throw new Error(`Database not found for modelName: ${modelName}`);
+        }
         let modelExists = await models[modelName].db.db.listCollections({
             name: collectionName
         }).toArray();
